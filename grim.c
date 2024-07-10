f641d71ddf1f9700f0222afdef8189de07660864
Geonwoo Kim <geonwkim@c1r7s5.42tokyo.jp>
2024-07-10 15:46:19 +0900

    [Update] - Updated main.c grim.c

--- grim.c
+++ grim.c
@@ -6,18 +6,24 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 23:13:25 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/07/09 23:19:41 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/07/10 15:45:36 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	pmu(pthread_mutex_t *p)
+int	pt_mutex_unlock(pthread_mutex_t *p)
 {
 	return (pthread_mutex_unlock(p));
 }
 
 void	*grim(void *p_i)
+{
+	grim_philo((t_philo **philo_info)p_i);
+	return (NULL);
+}
+
+void	*grim_philo(t_philo **philo_info)
 {
 	struct timeval	now;
 	struct timeval	lastmeal;
@@ -26,18 +32,18 @@ void	*grim(void *p_i)
 	while (1)
 	{
 		i = 0;
-		while (i < (*(t_philo **)p_i)[0].unified->n_philo)
+		while (i < (*(philo_info))[0].unified->n_philo)
 		{
-			if (philosophers_rw_end((*(t_philo **)p_i), 'r') == 1)
+			if (philosophers_rw_end((*(philo_info)), 'r') == 1)
 				return (NULL);
-			philosophers_rw_time(&((*(t_philo **)p_i)[i]), 'r', &lastmeal);
+			philosophers_rw_time(&((*(philo_info))[i]), 'r', &lastmeal);
 			gettimeofday(&(now), NULL);
-			if (get_elspsed_time(lastmeal, now) > (*(t_philo **)p_i)[i].timedie)
+			if (get_elspsed_time(lastmeal, now) > (*(philo_info))[i].timedie)
 			{
-				philosophers_print(&((*(t_philo **)p_i)[i]), "died", 1);
+				philosophers_print(&((*(philo_info))[i]), "died", 1);
 				i = -1;
-				while (++i < (*(t_philo **)p_i)[0].unified->n_philo)
-					pmu(&(((*(t_philo **)p_i)->unified->fork)[i]));
+				while (++i < (*(philo_info))[0].unified->n_philo)
+					pt_mutex_unlock(&(((*(philo_info))->unified->fork)[i]));
 				return (NULL);
 			}
 			i++;
